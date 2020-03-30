f664b1608a4c26b7d851a02397bf6288b637a3bc
longlu <longlu@511314.fun>
2020-03-30 19:01:42 +0800

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,39 @@
 const router = require('koa-router')()
 const multiparty = require('multiparty');
 const fs = require('fs')
+const path = require('path')
 
-
-const imgSavePath = 'service/files/md5.txt'
-let chunkNum = 0
+const md5FilePath = path.join('service/files/md5.json')
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index.html')
 })
 
-router.post('/fileCheck', async (ctx, next) => {
+router.post('/fileCheck', (ctx, next) => {
   const form = new multiparty.Form();
-  const md5s = await fs.readFileSync(imgSavePath, 'utf8')
-  const md5sArr = md5s.split(',')
+  createFile(md5FilePath)
+  let md5Obj = fs.readFileSync(md5FilePath, 'utf8') || '{}'
+  md5Obj = JSON.parse(md5Obj)
+  const md5Arr = Object.keys(md5Obj)
 
   return new Promise(reolve => {
     form.parse(ctx.req, function (err, fields, files) {
       if (err) { throw err; return; }
-      const uploadMd5 = fields['md5'][0]
-      chunkNum = fields['chunkNum'][0]
+      const md5 = fields['md5'][0]
+      const chunkNum = fields['chunkNum'][0]
       let status = 200
-      if (!(md5sArr.indexOf(uploadMd5) > -1)) {
+      //检验 MD5 与 已上传数量
+      if (!(md5Arr.indexOf(`${md5}`) > -1)) {
         status = 201
-        fs.appendFile(imgSavePath, `${uploadMd5},`, 'utf8', (e) => {
-          if (e) { throw e; return; }
-          console.log('md5已存储，开始上传' + uploadMd5);
-        })
+        md5Obj = {
+          ...md5Obj,
+          [md5]: []
+        }
+        fs.writeFileSync(md5FilePath, JSON.stringify(md5Obj))
+      } else if (md5Obj[md5].length !== chunkNum) {//断点续传
+        status = 201
       }
+
       ctx.body = {
         status
       }
@@ -35,25 +41,37 @@ router.post('/fileCheck', async (ctx, next) => {
     });
   })
 })
+
+const uploadMap = {
+  // [fileName]:{
+  // createWriteStream,
+  // current[]
+  // }
+}
+
 router.post('/fileUpload', async (ctx, next) => {
   const form = new multiparty.Form();
   await new Promise((resolve, reject) => {
     const status = 200
-    form.parse(ctx.req, function (err, fields, files) {
+    form.parse(ctx.req, async function (err, fields, files) {
       if (err) { throw err; return; }
-      // console.log(fields);//除文件外的其他附带信息
-      // console.log(files);//文件信息
+      // console.log(fields);
+      // console.log(files);
       const name = fields.name[0]
+      const md5 = fields.md5[0]
       const file = files.file[0]
-      console.log(name);
-      console.log(file);
-      console.log(chunkNum);
-      const writerStream = fs.createWriteStream(imgSavePath);
-      var bu = fs.createReadStream(file.path);
-      writerStream.pipe(bu)
+      const chunkNum = fields.chunkNum[0]
+      const current = fields.current[0]
 
-      bu.on('data', function (chunk) {
-        console.log(chunk.toString());//这是结果
+      if (current == 0) {
+        uploadMap[md5] = {}
+        uploadMap[md5]['createWriteStream'] = fs.createWriteStream(path.join('service/files/', name))
+      }
+      const getFile = fs.createReadStream(file.path);
+      getFile.pipe(uploadMap[md5]['createWriteStream'], { end: false })
+      getFile.on('data', function (chunk) {
+        // console.log('图片保存成功');
+        // fs.writeFileSync(md5FilePath, JSON.stringify(md5Obj2))
       });
 
       setTimeout(() => {
@@ -61,37 +79,16 @@ router.post('/fileUpload', async (ctx, next) => {
           status
         }
         resolve()
-      }, 1000);
+      }, 500);
     });
   })
 })
-// 合并分片
-function mergeChunks(fileName, chunks, callback) {
-  console.log('chunks:' + chunks);
-  let chunkPaths = chunks.map(function (name) {
-    return path.join(imgSavePath, name)
-  });
 
-  // 采用Stream方式合并
-  let targetStream = fs.createWriteStream(path.join(imgSavePath, fileName));
-
-  const readStream = function (chunkArray, cb) {
-    let path = chunkArray.shift();
-    let originStream = fs.createReadStream(path);
-    originStream.pipe(targetStream, { end: false });
-    originStream.on("end", function () {
-      // 删除文件
-      fs.unlinkSync(path);
-      if (chunkArray.length > 0) {
-        readStream(chunkArray, callback)
-      } else {
-        cb()
-      }
-    });
-  };
-
-  readStream(chunkPaths, callback);
-
+function createFile(path) {
+  const result = fs.existsSync(path);
+  if (!result) {
+    fs.writeFileSync(path, `{}`)
+  }
 }
 
 
